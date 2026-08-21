2678007f0a1b12e1365b8e80bfa191e2a5977be7
maniknt87 <vroy1187@gmail.com>
2026-08-21 22:51:02 +0530

    Handle nested resources and Key Vault recovery during destroy

--- backend/terraform/azure/main.tf
+++ backend/terraform/azure/main.tf
@@ -10,7 +10,16 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
